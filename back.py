787e6dfd0493284c267351cfc1cf2e5938c850a8
Igor <nikitinigorra@gmail.com>
2023-09-11 08:56:33 +0300

    logger fixed, no errors

--- back.py
+++ back.py
@@ -4,13 +4,20 @@ import logging
 
 #klaidu gaudymas -- Igor
 
-logging.basicConfig(
-    log_name="logger.log", 
-    encoding="UTF-8", 
-    level=logging.INFO,
-    format='%(asctime)s | %(name)s | %(levelname)s | %(message)s',
-)
-logging.info("Program has started")
+def create_logger(logger_name, log_file):
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.INFO)
+
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+
+    return logger
+
 
 class Task:
     def __init__(self, name: str, description: str, deadline: datetime, start_time=datetime.now(), status='in progress'):
@@ -27,46 +34,48 @@ class Task:
 class Entries:
     def __init__(self):
         self.task_list = []
-        logging.info("Main GUI created")
+        new_logger = create_logger("new_logger", "new_logger.log")
+        self.new_logger = new_logger
+        self.new_logger.info("Main GUI created")
 
     def add_task(self, task):
         self.task_list.append(task)
-        logging.info(f"Task created: {Task.__str__()}")
+        self.new_logger.info(f"Task created: {task}")
 
     def delete_task(self, task_name):
         try:
             for task in self.task_list:
                 if task.name == task_name:
                     self.task_list.remove(task)
-                    logging.info(f"Task deleted: {self.task_list(task)}")
+                    self.logger.info(f"Task deleted: {self.task_list(task)}")
         except:
             print(f"Selected task doesn't exist")
-            logging.info(f"Couldn't delete task, task not found")
+            self.new_logger.info(f"Couldn't delete task, task not found")
 
     def save_to_pickle(self, filename):
         with open (filename, 'wb') as file:
             pickle.dump(self.task_list, file)
-        logging.info(f"Data saved to file: {filename}")
+        self.new_logger.info(f"Data saved to file: {filename}")
             
     def load_from_pickle(self, filename):
         try:
             with open(filename, 'rb') as file:
                 self.task_list = pickle.load(file)
-            logging.info(f"Data loaded from file: {filename}")
+            self.new_logger.info(f"Data loaded from file: {filename}")
         except:
             self.task_list = []
-            logging.info(f"File not found, creating empty file")
+            self.new_logger.info(f"File not found, creating empty file")
 
     def change_status(self, task_name, new_status):
         for task in self.task_list:
             if task.name == task_name:
-                logging.info(f"Tasks {task} status changed from {task.status} to {new_status}")
+                self.new_logger.info(f"Tasks {task} status changed from {task.status} to {new_status}")
                 task.status = new_status
 
     def change_deadline(self, task_name, new_deadline):
         for task in self.task_list:
             if task.name == task_name:
-                logging.info(f"Task {task} deadline changed from {task.deadline} to {new_deadline}")
+                self.new_logger.info(f"Task {task} deadline changed from {task.deadline} to {new_deadline}")
                 task.deadline == new_deadline
         
                 
